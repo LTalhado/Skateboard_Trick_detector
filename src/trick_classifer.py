4690be884bde131836bdee85109bf29f70fe88ef
loco-c <carloscustodio9@gmail.com>
2024-05-23 22:45:34 +0100

    upload data and small changes

--- src/trick_classifer.py
+++ src/trick_classifer.py
@@ -1,7 +1,46 @@
+'''
+'''
 import torch
 import numpy as np
 import pandas as pd
 import torch.nn as nn
 
+device = "mps" if torch.backends.mps.is_available() else "cpu"
+# print(f"Using device: {device}")
+
+# Parameters
+BATCH_SIZE = None
+Learning_rate = 1e-3
+
+# normailize data for images  
+# data / 255
+# Frame size of video [244,244]
+
+import torch.nn as nn
+
+class Model(nn.Module):
+  def __init__(self):
+    super(Model, self).__init__()
+
+    # Define the convolutional layer with 3 input channels (RGB)
+    self.conv1 = nn.Conv3d(3, 16, kernel_size=3, padding=1)
+
+  def forward(self, x):
+    # Pass the input data (x) through the convolutional layer
+    x = self.conv1(x)
+    # You can add more layers here (e.g., pooling, activation)
+    # ...
+    return x
 
 
+model = Model()
+
+
+# loss Function
+
+# Training loop
+
+
+# Save model
+# PATH = ''
+# torch.save(model.state_dict(), PATH)
